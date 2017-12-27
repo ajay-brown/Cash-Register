a94becd3102d686c295536fd47bde2bd81319b32
Audrey <audreybrown94@gmail.com>
2017-12-27 10:12:14 -1000

    buttons made

---
+++ js/calculator.js
@@ -0,0 +1,121 @@
+/**
+ * Declare a function named `calculatorModule`
+ * this function will have two private variables declared inside of it.
+ * @variable PRIVATE { Number } `memory`
+ * @variable PRIVATE { Number } `total`
+ * @return {object} `calculator` object that can be used
+ */
+function calculatorModule() {
+    let memory = 0;
+    let total = 0;
+    
+     /**
+ * sets the `total` to the number passed in
+ * @param  { Number } x
+ * @return { Number }    current total
+ */   
+
+
+let load = function(x) {
+    validation(x);
+        total = x;
+        return total;
+        };
+  /**
+   * Validation
+   */
+  let validation = function(x) {
+    if (isNaN(x)) {
+        throw new Error(x + " is not a number!");
+      } 
+  }
+
+  /**
+   * Return the value of `total`
+   * @return { Number }
+   */
+let getTotal = function(){
+    return total;
+
+}
+
+  /**
+   * Sums the value passed in with `total`
+   * @param { Number } x
+   */
+
+   let add = function(x) {
+       validation(x);
+       return total += x
+   }
+
+  /**
+   * Subtracts the value passed in from `total`
+   * @param  { Number } x
+   */
+  let subtract = function(x) {
+      validation(x);
+      return total -= x
+  }
+
+
+  /**
+   * Multiplies the value by `total`
+   * @param  { Number } x
+   */
+
+   let multiply = function(x) {
+       validation(x);
+       return total *= x
+   }
+
+
+  /**
+   * Divides the value passing in by `total`
+   * @param  { Number } x
+   */
+let divide = function(x) {
+    validation(x);
+    return total /= x //should be reversed num divided by total or: x /= total or reword question: total divided by number
+
+}
+
+  /**
+   * Return the value stored at `memory`
+   * @return { Number }
+   */
+let recallMemory = function() {
+    return memory;
+}
+
+  /**
+   * Stores the value of `total` to `memory`
+   */
+let saveMemory = function () {
+    memory = total;
+    
+}
+
+  /**
+   * Clear the value stored at `memory`
+   */
+
+   let clearMemory = function(){
+       memory = 0;
+   }
+
+  return calculator = { //modules go here
+    load: load,
+    getTotal: getTotal,
+    add: add, 
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+    recallMemory: recallMemory,
+    saveMemory: saveMemory,
+    clearMemory: clearMemory,
+
+    }
+}
+//let myCalculator = calculatorModule()
+
---
+++ js/cash_register.js
@@ -0,0 +1,50 @@
+let calculator = calculatorModule()
+calculator.load
+let display = document.getElementById("display").innerHTML = "00.00";
+/////////
+let clickSeven = document.getElementById("seven").onclick = function(x) {
+    let x = 7
+    
+};
+
+let clickEight = document.getElementById("eight");
+
+let clickNine=document.getElementById("nine");
+
+let funcDivide = document.getElementById("divide");
+
+let clearDisp = document.getElementById("clear");
+/////////
+let clickFour = document.getElementById("four");
+
+let clickFive = document.getElementById("five");
+
+let clickSix = document.getElementById("six");
+
+let funcMult = document.getElementById("multiply");
+
+let getBalance = document.getElementById("balance");
+/////////
+let clickOne = document.getElementById("one");
+
+let clickTwo = document.getElementById("two");
+
+let clickThree = document.getElementById("three");
+
+let funcSubtract = document.getElementById("subtract");
+
+let deposit = document.getElementById("deposit");
+/////////
+let zero = document.getElementById("zero");
+
+let zeroes = document.getElementById("zeroes");
+
+let dec = document.getElementById("decimal");
+
+let funcAdd = document.getElementById("add");
+
+let withdraw = document.getElementById("withdraw");
+/////////
+let equals = document.getElementById("equals");
+
+
